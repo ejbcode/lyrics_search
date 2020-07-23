9188d4580ac9fa8c5e2d35a72409110933ac643a
Ernesto B <ejbcode@gmail.com>
2020-07-23 01:29:26 -0400

    Updates

---
+++ precache-manifest.4d22aea2bf042ee7fce9369da201b061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a625fd61bc6806eeb440abdbb5567175",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82581484af815d08723c",
+    "url": "/static/css/main.db461e4c.chunk.css"
+  },
+  {
+    "revision": "76e199375f77e3ccc4bc",
+    "url": "/static/js/2.845f1a73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.845f1a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82581484af815d08723c",
+    "url": "/static/js/main.3d1fc475.chunk.js"
+  },
+  {
+    "revision": "a0b35ee528f6f20daa74",
+    "url": "/static/js/runtime-main.399077f2.js"
+  }
+]);
